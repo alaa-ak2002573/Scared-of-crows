09b19549ba014d37bec59b74c64a35f868753580
beingfajr <beingfajer@gmail.com>
2026-04-24 02:10:36 +0300

    level 3 crows initial behaviour

---
+++ Scared-of-crows/Assets/Scripts/CrowCircle.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CrowCircle : MonoBehaviour
+{
+    [Header("Circle Settings")]
+    private float radius = 20f;
+    private float speed = 30f;
+    private float height = 50f;
+
+    private float angle = 0f;
+    private Vector3 centerPoint;
+    public float startAngle = 0f;
+
+    void Start()
+    {
+        centerPoint = transform.position;
+        angle = startAngle;
+    }
+
+    void Update()
+    {
+        angle += speed * Time.deltaTime;
+
+        float x = centerPoint.x + Mathf.Cos(angle * Mathf.Deg2Rad) * radius;
+        float z = centerPoint.z + Mathf.Sin(angle * Mathf.Deg2Rad) * radius;
+
+        transform.position = new Vector3(x, height, z);
+        transform.LookAt(new Vector3(x + Mathf.Cos((angle + 90) * Mathf.Deg2Rad), height, z + Mathf.Sin((angle + 90) * Mathf.Deg2Rad)));
+    }
+}
